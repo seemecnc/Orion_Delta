0c30fad11e45e948d49ca593a6203cfe7b0b63c5
John Oly <john@seemecnc.com>
2013-09-26 13:55:42 -0400

    adjusted configuration.h
    Adjusted configuration.h to match machines as shipping

--- FIRMWARE/Repetier/Configuration.h
+++ FIRMWARE/Repetier/Configuration.h
@@ -666,9 +666,9 @@ on this endstop.
 // For delta robot Z_MAX_LENGTH is maximum travel of the towers and should be set to the distance between the hotend
 // and the platform when the printer is at its home position.
 // If EEPROM is enabled these values will be overidden with the values in the EEPROM
-#define X_MAX_LENGTH 239.8  
-#define Y_MAX_LENGTH 239.8
-#define Z_MAX_LENGTH 239.8
+#define X_MAX_LENGTH 250    
+#define Y_MAX_LENGTH 250
+#define Z_MAX_LENGTH 250
 
 // Coordinates for the minimum axis. Can also be negative if you want to have the bed start at 0 and the printer can go to the left side
 // of the bed. Maximum coordinate is given by adding the above X_MAX_LENGTH values.
